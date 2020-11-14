431c192ce2dd1b0f7a915ff5f6dbcc4b23ffaf44
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-11-14 16:11:03 -0300

    Update iniciar_keycloak.sh

--- iniciar_keycloak.sh
+++ iniciar_keycloak.sh
@@ -3,3 +3,6 @@ docker-compose ps
 
 docker-compose up -d keycloak
 docker-compose ps
+
+docker exec -it keycloak keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user admin
+docker exec -it keycloak keycloak/bin/kcadm.sh update realms/master -s sslRequired=NONE
