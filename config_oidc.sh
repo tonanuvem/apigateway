975772c65c9ce53e8434503c75f206d8592cc8a2
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-11-18 12:35:45 -0300

    Create config_oidc.sh

---
+++ config_oidc.sh
@@ -0,0 +1,19 @@
+while [[ "$$(curl --connect-timeout 2 -s -o /dev/null -w ''%{http_code}'' http://localhost:8180)" != "200" ]]; do 
+  echo . 
+  sleep 5
+done 
+echo "Keycloak is up"
+
+HOST_IP=$(curl checkip.amazonaws.com)
+echo $HOST_IP
+
+# CLIENT_SECRET=<client_secret_from_keycloak>
+echo "Digite o secret do Kong:"
+read CLIENT_SECRET
+
+curl -s -X POST http://localhost:8001/plugins \
+  -d name=oidc \
+  -d config.client_id=kong \
+  -d config.client_secret=${CLIENT_SECRET} \
+  -d config.discovery=http://${HOST_IP}:8180/auth/realms/master/.well-known/openid-configuration \
+  | python -mjson.tool
