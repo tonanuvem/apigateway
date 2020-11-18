1ab15ef54e43099137adff915798086e2ae9945f
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-11-18 09:25:36 -0300

    Update iniciar_kong.sh

--- iniciar_kong.sh
+++ iniciar_kong.sh
@@ -1,12 +1,4 @@
 docker-compose up -d kong-db
-docker-compose ps
-
-docker-compose run --rm kong kong migrations up
-
+docker-compose up -d kong-migration
 docker-compose up -d kong
 docker-compose ps
-
-curl -s -X POST http://localhost:8001/services -d name=mock-service -d url=http://mockbin.org/request
-curl -s -X POST http://localhost:8001/routes -d service.id=${service_id} -d paths[]=/mock
-
-curl -s http://localhost:8000/mock
