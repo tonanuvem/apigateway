f4658f1264182a962d2eea8921599aff00373797
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2020-12-01 18:00:27 -0300

    Update config_kong.sh

--- config_kong.sh
+++ config_kong.sh
@@ -1,7 +1,5 @@
-while [[ "$$(curl --connect-timeout 2 -s -o /dev/null -w ''%{http_code}'' http://localhost:8001)" != "200" ]]; do 
-  echo . 
-  sleep 5
-done 
+#!/bin/bash
+bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8001)" != "200" ]]; do echo .; sleep 5; done'
 echo "Kong is up"
 
 curl -s -X POST http://localhost:8001/services -d name=mockbin -d url=http://mockbin.org/request
