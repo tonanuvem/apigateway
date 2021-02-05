85abb6c0cb0d5410c1bf30fc91072c41a1c3c377
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2021-02-04 22:53:17 -0300

    Update criar_rota_clientes.sh

--- criar_rota_clientes.sh
+++ criar_rota_clientes.sh
@@ -1,3 +1,3 @@
 IP=$(curl checkip.amazonaws.com)
-curl -i -X POST --url http://localhost:8001/services/ --data 'name=clientes' --data 'url=http://$IP:5000/api/clientes'
+curl -i -X POST --url http://localhost:8001/services/ --data 'name=clientes' --data "url=http://$IP:5000/api/clientes"
 curl -i -X POST --url http://localhost:8001/services/clientes/routes --data 'paths[]=/clientes'
