0b85810a17da7885fae3ed2f1a2d89831a148255
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-09-29 16:33:51 -0300

    Update iniciar_keycloak.sh

--- iniciar_keycloak.sh
+++ iniciar_keycloak.sh
@@ -3,3 +3,12 @@ docker-compose ps
 
 docker-compose up -d keycloak
 docker-compose ps
+echo ""
+echo "Config OK"
+echo ""
+IP=$(curl -s checkip.amazonaws.com)
+echo ""
+echo "URLs do projeto:"
+echo ""
+echo " - KeyCloak        : http://$IP:8180"
+echo ""
