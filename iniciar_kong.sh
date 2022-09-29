128467d2d6131b9750b20829fa82d45e166dda63
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2022-09-29 16:32:38 -0300

    Update iniciar_kong.sh

--- iniciar_kong.sh
+++ iniciar_kong.sh
@@ -3,3 +3,12 @@ docker-compose up -d kong-migration
 docker-compose up -d kong
 docker-compose up -d konga
 docker-compose ps
+echo ""
+echo "Config OK"
+echo ""
+IP=$(curl -s checkip.amazonaws.com)
+echo ""
+echo "URLs do projeto:"
+echo ""
+echo " - KONGA        : http://$IP:31337"
+echo ""
