3a3e9b48fe750b55c77881765d7935a7676e9b09
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-06-15 16:06:31 -0300

    Create bluegreen.sh

---
+++ bluegreen.sh
@@ -0,0 +1,7 @@
+echo "URLs do projeto:"
+echo ""
+echo " - ALUNO (BLUE)        : http://aluno.$IP.nip.io:8000"
+echo ""
+echo ""
+echo " - PROF  (GREEN)       : http://aluno.$IP.nip.io:8000"
+echo ""
