65f7c58adcdf81c2e374fb683590dc54a51e84ec
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-06-15 16:16:41 -0300

    Update bluegreen.sh

--- bluegreen.sh
+++ bluegreen.sh
@@ -1,3 +1,9 @@
+#curl -X POST http://localhost:8001/services/ --data "name=bluegreen-aluno" --data "url=http://$IP:8081"
+#curl -i -X POST --url http://localhost:8001/services/bluegreen-aluno/routes --data "hosts[]=aluno.$IP.nip.io"
+
+#curl -X POST http://localhost:8001/services/ --data "name=bluegreen-prof" --data "url=http://$IP:8082"
+#curl -i -X POST --url http://localhost:8001/services/bluegreen-prof/routes --data "hosts[]=prof.$IP.nip.io"
+
 IP=$(curl checkip.amazonaws.com) 
 echo "URLs do projeto:"
 echo ""
