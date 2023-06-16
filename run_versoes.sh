16433ab3bcdb575c28085d99c2c6c3cee5e2516e
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2023-06-15 21:27:48 -0300

    Update run_versoes.sh

--- run_versoes.sh
+++ run_versoes.sh
@@ -7,13 +7,6 @@ curl -i -X POST --url http://localhost:8001/services/bluegreen-aluno/routes --da
 curl -X POST http://localhost:8001/services/ --data "name=bluegreen-prof" --data "url=http://$IP:8082"
 curl -i -X POST --url http://localhost:8001/services/bluegreen-prof/routes --data "hosts[]=prof.$IP.nip.io"
 IP=$(curl checkip.amazonaws.com) 
-echo "URLs do projeto:"
-echo ""
-echo " - ALUNO (BLUE)        : http://aluno.$IP.nip.io:8000"
-echo ""
-echo ""
-echo " - PROF  (GREEN)       : http://prof.$IP.nip.io:8000"
-echo ""
 
 ## CANARY
 curl -X POST http://localhost:8001/services/ --data "name=notificacao-canary" --data "host=notificacao.upstream"
@@ -21,8 +14,7 @@ curl -i -X POST --url http://localhost:8001/services/notificacao-canary/routes -
 curl -X POST http://localhost:8001/upstreams --data 'name=notificacao.upstream'
 curl -X POST http://localhost:8001/upstreams/notificacao.upstream/targets --data "target=$IP:8081" --data "weight=70" 
 curl -X POST http://localhost:8001/upstreams/notificacao.upstream/targets --data "target=$IP:8082" --data "weight=30" 
-IP=$(curl checkip.amazonaws.com) 
-echo "URLs do projeto:"
-echo ""
-echo " - CANARY (% percentual)  : http://$IP:8000/notificacao/canary"
-echo ""
+
+## URLs:
+sh bluegreen.sh
+sh canary.sh
