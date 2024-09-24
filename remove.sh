fc06b6caf2bb04299c5576972f65e515e1463aa4
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-09-24 11:48:57 -0300

    Update remove.sh

--- remove.sh
+++ remove.sh
@@ -1,2 +1,3 @@
 docker stop notificacao blue green clientes && docker rm notificacao blue green clientes 
+docker volume rm apigateway_kong-datastore apigateway_keycloak-datastore
 docker-compose stop && docker-compose rm -f && docker volume prune -f && git pull
