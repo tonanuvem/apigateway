9e0149bd62f5a16987e8aee64d3a54290e2dd992
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-09-24 10:12:40 -0300

    Update config_keycloak.sh

--- config_keycloak.sh
+++ config_keycloak.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' localhost:8180)" != "200" ]]; do echo .; sleep 5; done'
+bash -c 'while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' http://localhost:8180/admin/master/console/)" != "200" ]]; do echo .; sleep 5; done'
 echo "Keycloak is up"
 
-docker-compose exec keycloak keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user admin --password admin
-docker-compose exec keycloak keycloak/bin/kcadm.sh update realms/master -s sslRequired=NONE --user admin --password admin
+export PASSWORD=admin
+docker-compose exec keycloak opt/keycloak/bin/kcadm.sh config credentials --server http://localhost:8080/auth --realm master --user admin --password $PASSWORD
+docker-compose exec keycloak opt/keycloak/bin/kcadm.sh get realms --fields id,realm,enabled
+docker-compose exec keycloak opt/keycloak/bin/kcadm.sh update realms/master -s sslRequired=NONE --user admin --password admin
