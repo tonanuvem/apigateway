1c6faccb50d4df2f1cdc259e760b0d7ffe6ec4e8
tonanuvem <38731039+tonanuvem@users.noreply.github.com>
2024-09-24 16:01:16 -0300

    Update run_versoes.sh

--- run_versoes.sh
+++ run_versoes.sh
@@ -1,12 +1,20 @@
+docker run --name login -p 8070:80 -d tonanuvem/login
 docker run --name blue -p 8081:80 -d tonanuvem/fiap_page:blue
 docker run --name green -p 8082:80 -d tonanuvem/fiap_page:green
 
+IP=$(curl checkip.amazonaws.com) 
+
 ## BLUE GREEN
+# LOGIN
+curl -X POST http://localhost:8001/services/ --data "name=bluegreen-login" --data "url=http://$IP:8070"
+curl -i -X POST --url http://localhost:8001/services/bluegreen-login/routes --data "hosts[]=$IP.sslip.io"
+# BLUE
 curl -X POST http://localhost:8001/services/ --data "name=bluegreen-aluno" --data "url=http://$IP:8081"
 curl -i -X POST --url http://localhost:8001/services/bluegreen-aluno/routes --data "hosts[]=aluno.$IP.sslip.io"
+#  GREEN
 curl -X POST http://localhost:8001/services/ --data "name=bluegreen-prof" --data "url=http://$IP:8082"
 curl -i -X POST --url http://localhost:8001/services/bluegreen-prof/routes --data "hosts[]=prof.$IP.sslip.io"
-IP=$(curl checkip.amazonaws.com) 
+
 
 ## CANARY
 curl -X POST http://localhost:8001/services/ --data "name=notificacao-canary" --data "host=notificacao.upstream"
